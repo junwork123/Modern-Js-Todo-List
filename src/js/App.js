db75ab128ef6cbb9b5e96dec3531f102b7c1b0a2
Junhyeok Choi <junwork123@gmail.com>
2023-04-08 23:53:49 +0900

    feat: 토글 변경 시 상태가 변경되도록 ChangeTodo 컴포넌트 작성

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,8 @@
 import { $ } from "./utils/selector.js";
 import AddTodo from "./components/AddTodo.js";
 import ViewTodo from "./components/ViewTodo.js";
+import ChangeTodo from "./components/ChangeTodo.js";
+
 export default class App {
     constructor() {
         this.todoList = $('.todo-list');
@@ -8,5 +10,6 @@ export default class App {
         this.newTodoTitle = $('.new-todo');
         this.viewTodo = new ViewTodo(this.todoList, this.todoCount);
         this.addTodo = new AddTodo(this.newTodoTitle, this.viewTodo);
+        this.changeTodo = new ChangeTodo(this.todoList, this.viewTodo);
     }
 }
---
+++ src/js/components/ChangeTodo.js
@@ -0,0 +1,28 @@
+export default class ChangeTodo {
+    constructor($todoList, viewTodo) {
+        this.$todoList = $todoList;
+        this.viewTodo = viewTodo;
+        this.mount();
+    }
+
+    mount() {
+        this.$todoList.addEventListener('click', this.changeTodo.bind(this));
+    }
+
+    changeTodo = ({ target }) => {
+        this.$todos = JSON.parse(localStorage.getItem('todos'));
+        if(target.classList.contains('toggle')){
+           return this.toggleTodo(target);
+        }
+    }
+
+    toggleTodo = (target) => {
+        this.$todos.map((todo) => {
+           if(todo.id === target.id){
+               todo.completed = !todo.completed;
+           }
+        });
+        localStorage.setItem('todos', JSON.stringify(this.$todos));
+        this.viewTodo.loadTodo();
+    }
+}

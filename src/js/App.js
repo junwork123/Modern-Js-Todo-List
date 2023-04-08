ec6ae58febf3e432a823b1b5af42f3cb7eab34f5
Junhyeok Choi <junwork123@gmail.com>
2023-04-08 22:03:15 +0900

    feat: 화면 갱신 함수 작성

--- src/js/App.js
+++ src/js/App.js
@@ -1,7 +1,34 @@
 
 export default class App {
     constructor() {
-        console.log('App is running');
+        this.$todoList = document.querySelector('.todo-list');
+        this.$todoCount = document.querySelector('.todo-count strong');
+        this.loadTodo();
+    }
+    todoTemplate = ({ id, title, completed }) => {
+        return `<li id=${id} class=${completed && 'completed'} >
+                <div class="view">
+                    <input class="toggle" type="checkbox" 
+                      id=${id} ${completed && 'checked'} />
+                    <label class="label">${title}</label>
+                    <button class="destroy" id=${id}></button>
+                </div>
+                <input class="edit" value="${title}" />
+            </li>`;
+    };
+
+    viewAll = () => {
+        this.todos.map((todo) => {
+            // insertAdjacentHTML는 DOM을 새로 그리지 않고 요소를 추가한다.
+            this.$todoList.insertAdjacentHTML('beforeend', this.todoTemplate(todo));
+        });
+    };
+
+    loadTodo = () => {
+        this.todos = JSON.parse(localStorage.getItem('todos')) ?? [];
+        this.$todoList.innerHTML = '';
+        this.viewAll();
+        this.$todoCount.innerHTML = this.$todoList.querySelectorAll('li').length;
     }
 }
 

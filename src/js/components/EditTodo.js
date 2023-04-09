4f2bf0249026a0a76d6ec36f1f082618f281c192
Junhyeok Choi <junwork123@gmail.com>
2023-04-09 12:59:00 +0900

    fix: 아이템 추가 시 필터 기능이 유지되도록 변경

--- src/js/components/EditTodo.js
+++ src/js/components/EditTodo.js
@@ -1,44 +1,61 @@
-export default class EditTodo {
-    constructor($todoList, viewTodo) {
+import { ALL, ACTIVE, COMPLETED } from "../contant/TodoStatus.js";
+
+export default class ViewTodo {
+    constructor( $todoList, $todoCount ) {
         this.$todoList = $todoList;
-        this.viewTodo = viewTodo;
-        this.mount();
-    }
-    mount() {
-        this.$todoList.addEventListener('dblclick', this.editTodo.bind(this));
-    }
-
-    editTodoItem = ({ target, key }, originalValue) => {
-        if(!target.value) {
-            return;
-        }
-        switch (key) {
-            case 'Enter':
-                return this.updateTodoItem(target.closest('li'), target.value);
-            case 'Escape':
-                target.value = originalValue;
-                return target.closest('li').classList.remove('editing');
-        }
+        this.$todoCount = $todoCount;
+        this.filter = ALL;
+        this.todos = [];
+        this.render();
     }
 
-    updateTodoItem = (updateId, value) => {
-        this.todos = this.todos.map((todo) => {
-            if (todo.id === updateId) {
-                todo.title = value;
-            }
-            localStorage.setItem('todos', JSON.stringify(this.todos));
-            this.viewTodo.render();
+    todoTemplate = ({ id, title, completed }) => {
+        return `<li id=${id} class=${completed && 'completed'} >
+                <div class="view">
+                    <input class="toggle" type="checkbox" 
+                      id=${id} ${completed && 'checked'} />
+                    <label class="label">${title}</label>
+                    <button class="destroy" id=${id}></button>
+                </div>
+                <input class="edit" value="${title}" />
+            </li>`;
+    };
+
+    renderFilteredTodoList = (filter) => {
+        switch (filter) {
+            case ALL:
+                this.renderAll(); break;
+            case ACTIVE:
+                this.renderActive(); break;
+            case COMPLETED:
+                this.renderCompleted(); break;
+        }
+    }
+    renderTodoList = (todoList) => {
+        todoList.map((todo) => {
+            // insertAdjacentHTML는 DOM을 새로 그리지 않고 요소를 추가한다.
+            this.$todoList.insertAdjacentHTML('beforeend', this.todoTemplate(todo));
         });
     }
+    renderAll = () => {
+        this.renderTodoList(this.todos);
+    };
+    renderActive = () => {
+        const activeTodos = this.todos.filter((todo) => !todo.completed);
+        this.renderTodoList(activeTodos);
+    }
+    renderCompleted = () => {
+        const completedTodos = this.todos.filter((todo) => todo.completed);
+        this.renderTodoList(completedTodos);
+    }
 
-    editTodo = ({ target }) => {
-        this.todos = JSON.parse(localStorage.getItem('todos'));
-        if(target.className === 'label'){
-            const originValue = target.innerText;
-            target.closest('li').classList.add('editing');
-            target
-                .closest('li')
-                .addEventListener('keyup', this.editTodoItem.bind(this, originValue));
+    render = (filter = ALL) => {
+        if( filter !== ALL ) {
+            this.filter = filter;
         }
+        this.todos = JSON.parse(localStorage.getItem('todos')) ?? [];
+        this.$todoList.innerHTML = '';
+        this.renderFilteredTodoList(this.filter);
+        this.$todoCount.innerHTML = this.$todoList.querySelectorAll('li').length;
     }
 }

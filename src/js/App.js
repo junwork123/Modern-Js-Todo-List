bde989d7d8fe8415c36ecedc3ffd1e5278d2f6fe
Junhyeok Choi <junwork123@gmail.com>
2023-04-09 11:59:00 +0900

    feat: 필터링 기능 추가(close #6)

--- src/js/App.js
+++ src/js/App.js
@@ -3,15 +3,18 @@ import AddTodo from "./components/AddTodo.js";
 import ViewTodo from "./components/ViewTodo.js";
 import ChangeTodo from "./components/ChangeTodo.js";
 import EditTodo from "./components/EditTodo.js";
+import FilterTodo from "./components/FilterTodo.js";
 
 export default class App {
     constructor() {
-        this.todoList = $('.todo-list');
-        this.todoCount = $('.todo-count strong');
-        this.newTodoTitle = $('.new-todo');
-        this.viewTodo = new ViewTodo(this.todoList, this.todoCount);
-        this.addTodo = new AddTodo(this.newTodoTitle, this.viewTodo);
-        this.changeTodo = new ChangeTodo(this.todoList, this.viewTodo);
-        this.editTodo = new EditTodo(this.todoList, this.viewTodo);
+        this.$todoList = $('.todo-list');
+        this.$todoCount = $('.todo-count strong');
+        this.$filters = $('.filters');
+        this.$newTodoTitle = $('.new-todo');
+        this.viewTodo = new ViewTodo(this.$todoList, this.$todoCount);
+        this.addTodo = new AddTodo(this.$newTodoTitle, this.viewTodo);
+        this.changeTodo = new ChangeTodo(this.$todoList, this.viewTodo);
+        this.editTodo = new EditTodo(this.$todoList, this.viewTodo);
+        this.filterTodo = new FilterTodo(this.$filters, this.viewTodo);
     }
 }

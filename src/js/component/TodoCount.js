71ebfa037fe0e5b3a772d5c1c0f7fd26750f25a2
Junhyeok Choi <junwork123@gmail.com>
2023-05-07 12:47:44 +0900

    feat: 할일 카운트 기능 추가

--- src/js/component/TodoCount.js
+++ src/js/component/TodoCount.js
@@ -1,16 +1,20 @@
 import Component from "../core/Component.js";
 import { store } from "../store/index.js";
 
-
+const renderTodoCount = (todos) => {
+    const count = todos && todos.length;
+    return '총 <strong>' + count + '</strong> 개';
+}
 export default class TodoCount extends Component {
     initState() { return {}; }
     mounted() {
         // 컴포넌트가 마운트된 후에 동작한다.
     }
     template() {
+        const { todos } = store.getState();
         return `
             <span class="todo-count">
-                총 <strong>0</strong> 개
+                ${renderTodoCount(todos)}
             </span>
                 <ul class="filters">
                     <li>
